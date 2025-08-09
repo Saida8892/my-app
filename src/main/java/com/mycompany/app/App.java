9da4ac3693ee015d39650212be18ad8764344a91
Saida8892 <saidareddyakkala18@gmail.com>
2025-08-09 14:09:31 +0530

    second commit

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -1,10 +1,10 @@
 package com.mycompany.app;
 
 /**
- * Hello !
+ * Hello devops !
  */
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello !");
+        System.out.println("Hello devops !");
     }
 }
